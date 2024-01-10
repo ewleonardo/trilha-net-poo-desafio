63a8ccf82b4dc7bb3accb0acbe81578a4db03055
ejunior <ewerton.junior14@Outlook.com>
2024-01-09 21:52:32 -0300

    Conclusão do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia Papai:");
+Smartphone nokia = new Nokia("13213131", "Nokia 12", "IMEI1323131", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Bíblia Sagrada");
+
+
+Console.WriteLine("\nIphone Papai:");
+Smartphone iphone = new Iphone("1232134", "Iphone 8", "IMEI99129321", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Bíblia Sagrada Plus");
